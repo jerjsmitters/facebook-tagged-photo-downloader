e831737eb927a083eb36c8e0094c64b7e76fd5e0
georgersmith <g.smith@proteo.co.uk>
2023-01-06 20:04:28 +0000

    Wait for page to load, tidyup

--- facebookPhotoDownloader.js
+++ facebookPhotoDownloader.js
@@ -5,33 +5,43 @@
 //---- INPUT PARAMS
 const makeItHumanlike = false;
 const isCalibrating = false; //don't save if we are calibrating
-const totalPhotoCount = 2;
-const delayBetweenSaves_ms = 10000; //we want it to be long enough that we can terminate the process if we need to (manually from cmd)
-const delayWaitForSaveModal_ms = 2000;
+const totalPhotoCount = 200;
 
 //---- CALIBRATION -----//
 const units = 20; //pixels
 
 const taskbarChrome_x = 23 * units;
-
 const toContextMenuSave_dx = 1 * units;
 const toContextMenuSave_dy = 2 * units;
-
 const toModalSaveButton_dx = 5 * units;
 const toModalSaveButton_dy = 5 * units;
 
 
-//while loop, while x < (number of photos)
+const delayBetweenSaves_ms = 10000; //we want it to be long enough that we can terminate the process if we need to (manually from cmd)
+const delayWaitForSaveModal_ms = 2000;
+const delayForPageToLoad_ms = 2000;
+
+
+// ------ SETUP/FNs ------ //
 const robot = require("robotjs");
 const screenDimensions = robot.getScreenSize();
 
-function moveMouseAbsolute(x, y){
-    makeItHumanlike ? robot.moveMouseSmooth(x, y, 1) : robot.moveMouse(x, y);
-}
+async function Program(){
+    //let's open chrome
+    const taskbarChrome_y = screenDimensions.height - 1* units; // taskbar y is slightly above bot of screen
+    robot.moveMouse(taskbarChrome_x, taskbarChrome_y);
+    robot.mouseClick();
 
-function moveMouseRelative(x, y){
-    var currentPosition = robot.getMousePos();
-    moveMouseAbsolute(currentPosition.x + x, currentPosition.y + y);
+    let counter = 0;
+    while (counter < totalPhotoCount){
+        await savePhoto();
+        await WaitAsync(delayBetweenSaves_ms);
+        navigateToNext();
+        await WaitAsync(delayForPageToLoad_ms);
+        
+        counter++;
+    }
+
 }
 
 async function savePhoto(){
@@ -58,27 +68,22 @@ function navigateToNext(){
     robot.keyTap("right")
 }
 
+function moveMouseAbsolute(x, y){
+    makeItHumanlike ? robot.moveMouseSmooth(x, y, 1) : robot.moveMouse(x, y);
+}
+
+function moveMouseRelative(x, y){
+    var currentPosition = robot.getMousePos();
+    moveMouseAbsolute(currentPosition.x + x, currentPosition.y + y);
+}
+
 async function WaitAsync(delayMs){
     await new Promise(resolve => setTimeout(resolve, delayMs));
 }
 
 
+
 //---- BEGIN SCRIPT ----//
-
-async function Program(){
-    //let's open chrome
-    const taskbarChrome_y = screenDimensions.height - 1* units; // taskbar y is slightly above bot of screen
-    robot.moveMouse(taskbarChrome_x, taskbarChrome_y);
-    robot.mouseClick();
-
-    let counter = 0;
-    while (counter < totalPhotoCount){
-        await savePhoto();
-        await WaitAsync(delayBetweenSaves_ms);
-        navigateToNext();
-        counter++;
-    }
-}
 
 Program();
 
